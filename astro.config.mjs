f13f256a552008870b03db2b601106bfd9b7b12b
0xm1sk <tnmed200@gmail.com>
2026-09-25 07:50:29 +0100

    Force Build: Global layout sync v1.0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://0xm1sk.github.io',
+  site: "https://0xm1sk.github.io",
   base: '/profilio',
   trailingSlash: 'always',
   integrations: [tailwind()],
